2432c3b4d4c984e3653a1b127317d2cbe3c57366
borysstach <borysstach@gmail.com>
2018-04-11 17:38:59 +0200

    Add navigation controller for easy navigation between tab stacks

--- app/src/main/java/pl/borys/decide/usecase/main/MainActivity.kt
+++ app/src/main/java/pl/borys/decide/usecase/main/MainActivity.kt
@@ -2,7 +2,7 @@ package pl.borys.decide.usecase.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_activity.*
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import pl.borys.decide.R
@@ -10,11 +10,13 @@ import pl.borys.decide.R
 
 class MainActivity : AppCompatActivity() {
 
+    private var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.main_activity)
         initBottomBar()
+        navController = NavController(savedInstanceState, supportFragmentManager)
     }
 
     private fun initBottomBar() {
@@ -29,25 +31,19 @@ class MainActivity : AppCompatActivity() {
                                 )
                         )
                     }
-            setOnTabSelectedListener(onNavigationItemSelectedListener)
+            setOnTabSelectedListener(bottomBarSelectListener)
         }
     }
 
-    private val onNavigationItemSelectedListener = AHBottomNavigation.OnTabSelectedListener { position, wasSelected ->
-        when (position) {
-            BottomBarTabsEnum.VOTE.ordinal -> {
-                message.setText(BottomBarTabsEnum.VOTE.title) //TODO: change to decide fragment
-                return@OnTabSelectedListener true
-            }
-            BottomBarTabsEnum.ASK.ordinal -> {
-                message.setText(BottomBarTabsEnum.ASK.title) //TODO: change to ask fragment
-                return@OnTabSelectedListener true
-            }
-            BottomBarTabsEnum.SOCIAL.ordinal -> {
-                message.setText(BottomBarTabsEnum.SOCIAL.title) //TODO: change to friends fragment
-                return@OnTabSelectedListener true
-            }
+    private val bottomBarSelectListener = AHBottomNavigation.OnTabSelectedListener { position, wasSelected ->
+        navController?.switchTab(position)
+        true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.let {
+            navController?.onSaveInstanceState(it)
         }
-        false
     }
 }
